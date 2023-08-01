c2f409d02e40ebb93232f043ac5d2af6c45b6a27
Leandro <leandro@live.de>
2023-08-01 17:39:35 -0300

    update final

--- main.mjs
+++ main.mjs
@@ -2,21 +2,19 @@ import { Database } from './database.mjs';
 
 const database = new Database();
 
-database
-  .execute(
-    'create table author (id number, name string, age number, city string, state string, country string)'
-  )
-  .then(function () {
-    return Promise.all([
+(async function () {
+  try {
+    await database.execute(
+      'create table author (id number, name string, age number, city string, state string, country string)'
+    );
+    await Promise.all([
       database.execute('insert into author (id, name, age) values (1, Douglas Crockford, 62)'),
       database.execute('insert into author (id, name, age) values (2, Linus Torvalds, 47)'),
       database.execute('insert into author (id, name, age) values (3, Martin Fowler, 54)'),
-    ]).then(function () {
-      return database.execute('select id, name, age from author').then(function (result) {
-        console.log(result);
-      });
-    });
-  })
-  .catch(function (error) {
+    ]);
+    const result = await database.execute('select id, name, age from author');
+    console.log(JSON.stringify(result, undefined, ' '));
+  } catch (error) {
     console.log(error);
-  });
+  }
+})();
